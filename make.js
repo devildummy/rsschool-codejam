e5d909be1343cbcee95a2748fb6f98fe9fa4f258
devildummy <cyrok.13@gmail.com>
2018-10-27 21:30:42 +0300

    feat: create new function - make

--- make.js
+++ make.js
@@ -1,3 +1,14 @@
-module.exports = make = (array) =>{
-
+module.exports = make = (...args) =>{
+    const store=[];
+    const reducer = (...args) =>{
+        if (args[0] instanceof Function){
+            return store.reduce(args[0]);
+        }
+        args.forEach((item)=>{
+            store.push(item);
+        });
+        
+        return reducer;
+    };
+    return reducer(...args);
 };
